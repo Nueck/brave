2a36dc5183e43b1cdc3cad8e389eee609adb8966
Nueck <nueck@outlook.com>
2023-03-15 17:49:11 +0800

    feat🚀：添加邮箱判断

--- brave-utils/src/common.rs
+++ brave-utils/src/common.rs
@@ -4,3 +4,13 @@ pub fn is_valid_email(email: &str) -> bool {
     let re = Regex::new(r"^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$").unwrap();
     re.is_match(email)
 }
+
+/*判断是否是outlook邮箱*/
+pub fn is_outlook_email(email: &str) -> bool {
+    let email_parts: Vec<&str> = email.split('@').collect();
+    if email_parts.len() != 2 {
+        return false;
+    }
+    let domain = email_parts[1].to_lowercase();
+    domain == "outlook.com"
+}
