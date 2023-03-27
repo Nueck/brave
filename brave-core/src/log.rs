bbf75b6c3f06ae9d8927f46495a3fd6d1f921dde
Nueck <nueck@outlook.com>
2023-03-27 22:11:16 +0800

    为log添加条件编译

--- brave-core/src/log.rs
+++ brave-core/src/log.rs
@@ -8,11 +8,16 @@ pub fn init_log() {
     //设置may的最小线程(由于fast_log使用的是may)
     may::config().set_workers(2);
 
+    #[cfg(debug_assertions)]
+    let level = LevelFilter::Info;
+    #[cfg(not(debug_assertions))]
+    let level = LevelFilter::Warn;
+
     fast_log::init(
         Config::new()
             .console()
             .file("log/brave-rust.log")
-            .level(LevelFilter::Debug)
+            .level(level)
             .chan_len(Some(100000)),
     )
     .unwrap();
