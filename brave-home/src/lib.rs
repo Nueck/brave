8ea4684426cb32bdb3f36eaa262aabc24b7dd18c
Nueck <nueck@outlook.com>
2023-03-27 23:44:07 +0800

    修复页面图标加载失败

--- brave-home/src/lib.rs
+++ brave-home/src/lib.rs
@@ -1,24 +1,22 @@
 mod index;
 
 use actix_files::Files;
-use actix_web::dev::{fn_service, ServiceRequest, ServiceResponse};
-use actix_web::http::header;
-use actix_web::{web, HttpResponse};
+use actix_web::web;
 
 /*用于首页显示配置的*/
 pub fn home_config(cfg: &mut web::ServiceConfig) {
     cfg.configure(index::index_config).service(
         Files::new("/", "./templates/")
             .index_file("index.html")
-            .default_handler(fn_service(|req: ServiceRequest| async {
-                let (req, _) = req.into_parts();
-                Ok(ServiceResponse::new(
-                    req,
-                    HttpResponse::Found()
-                        .append_header((header::LOCATION, "/"))
-                        .finish(),
-                ))
-            }))
+            // .default_handler(fn_service(|req: ServiceRequest| async {
+            //     let (req, _) = req.into_parts();
+            //     Ok(ServiceResponse::new(
+            //         req,
+            //         HttpResponse::Found()
+            //             .append_header((header::LOCATION, "/"))
+            //             .finish(),
+            //     ))
+            // }))
             .prefer_utf8(true),
     );
 }
