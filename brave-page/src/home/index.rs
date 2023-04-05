18fde5ff5496695c37a348a4da0a1c576ee1d415
Nueck <nueck@outlook.com>
2023-04-06 00:16:39 +0800

    暂时禁用首页用户名跳转

--- brave-page/src/home/index.rs
+++ brave-page/src/home/index.rs
@@ -1,5 +1,6 @@
+use actix_files::NamedFile;
 use actix_web::http::header;
-use actix_web::{get, web, HttpResponse, Responder, Result};
+use actix_web::{get, web, HttpRequest, HttpResponse, Responder, Result};
 use askama::DynTemplate;
 use askama::Template;
 use brave_config::app::AppState;
@@ -7,7 +8,7 @@ use brave_config::init::InitStatus;
 use brave_config::interface::Interface;
 use brave_db::entity::prelude::Users;
 use brave_db::entity::users;
-use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
+use sea_orm::{ColIdx, ColumnTrait, EntityTrait, QueryFilter};
 
 #[derive(Template)]
 #[template(path = "index.html")]
@@ -16,19 +17,16 @@ struct IndexTemplate<'a> {
 }
 
 pub fn index_config(cfg: &mut web::ServiceConfig) {
-    cfg.service(index).service(home_index);
+    cfg.service(index);
 }
 
 #[get("/")]
 async fn index() -> Result<impl Responder> {
-    /*根据初始化状态*/
     if InitStatus::global().is_init {
         let add = Interface::redirect_login_address();
-
         let html = IndexTemplate { admin_login: &add }.dyn_render().unwrap();
         Ok(HttpResponse::Ok().body(html))
     } else {
-        /*重定向到初始化*/
         let init_add = Interface::redirect_init_address();
         Ok(HttpResponse::Found()
             .append_header((header::LOCATION, init_add))
@@ -36,42 +34,47 @@ async fn index() -> Result<impl Responder> {
     }
 }
 
-/*增加地址的趣味性*/
-#[get("/{name}")]
-pub async fn home_index(
-    name: web::Path<String>,
-    data: web::Data<AppState>,
-) -> Result<impl Responder> {
-    /*根据初始化状态*/
-    if InitStatus::global().is_init {
-        //修复/blog下不重定向的问题
-
-        let db = &data.conn;
-
-        match Users::find()
-            .filter(users::Column::UserName.contains(&name))
-            .one(db)
-            .await
-            .expect("Could not find Users -- file load blog")
-        {
-            None => {
-                log::error!("{}", &name);
-                Ok(HttpResponse::Found()
-                    .append_header((header::LOCATION, "/"))
-                    .finish())
-            }
-            Some(_) => {
-                let name_blog = Interface::redirect_user_blog_home(&name);
-                Ok(HttpResponse::Found()
-                    .append_header((header::LOCATION, name_blog))
-                    .finish())
-            }
-        }
-    } else {
-        /*重定向到初始化*/
-        let init_add = Interface::redirect_init_address();
-        Ok(HttpResponse::Found()
-            .append_header((header::LOCATION, init_add))
-            .finish())
-    }
-}
+// //增加地址的趣味性 (图标问题)
+// #[get("/{name}")]
+// pub async fn home_index(
+//     path: web::Path<String>,
+//     data: web::Data<AppState>,
+//     req: HttpRequest,
+// ) -> Result<impl Responder> {
+//     if InitStatus::global().is_init {
+//         let db = &data.conn;
+//         let name = path.into_inner();
+//
+//         if name == "favicon.ico".to_owned() {
+//             return Ok(NamedFile::open_async("./templates/favicon.ico")
+//                 .await?
+//                 .into_response(&req));
+//         }
+//
+//         match Users::find()
+//             .filter(users::Column::UserName.contains(&name))
+//             .one(db)
+//             .await
+//             .expect("Could not find Users -- file load blog")
+//         {
+//             None => {
+//                 log::error!("{}", &name);
+//                 Ok(HttpResponse::Found()
+//                     .append_header((header::LOCATION, "/"))
+//                     .finish())
+//             }
+//             Some(_) => {
+//                 let name_blog = Interface::redirect_user_blog_home(&name);
+//                 Ok(HttpResponse::Found()
+//                     .append_header((header::LOCATION, name_blog))
+//                     .finish())
+//             }
+//         }
+//     } else {
+//         /*重定向到初始化*/
+//         let init_add = Interface::redirect_init_address();
+//         Ok(HttpResponse::Found()
+//             .append_header((header::LOCATION, init_add))
+//             .finish())
+//     }
+// }
